3e14f97156a92bc83d8cbcced65497153932f3cc
DingEn <dingen@etownol.biz>
2017-07-12 10:32:53 +0800

    20170712

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp')
+
+gulp.task("before", function(){
+    return gulp.src('./.travis/copy/*')
+        .pipe(gulp.dest('./'));
+})
+
+gulp.task("after", function(){
+    return gulp.src('./public/*')
+        .pipe(gulp.dest('./ydn2856_public/'));
+})
